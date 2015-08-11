4270687a75954bbccf8a1447487259d44f5d6953
Robert Vock <robert.vock@4wdmedia.de>
2015-08-11 14:26:03 +0200

    throw pageNotFound if requested extbase object cannot be found

--- Classes/Controller/ActionController.php
+++ Classes/Controller/ActionController.php
@@ -5,10 +5,13 @@ namespace Vierwd\VierwdSmarty\Controller;
 class ActionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
 	/**
+	 * @var string
+	 */
+	protected $entityNotFoundMessage = 'The requested entity could not be found.';
+
+	/**
 	 * this is needed to use the smarty view
 	 */
-	protected $viewObjectNamePattern = 'Vierwd\\VierwdSmarty\\View\\SmartyView';
-	protected $deprecatedViewObjectNamePattern = 'Vierwd\\VierwdSmarty\\View\\SmartyView';
 	protected $namespacesViewObjectNamePattern = 'Vierwd\\VierwdSmarty\\View\\SmartyView';
 
 	/**
@@ -22,5 +25,26 @@ class ActionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControlle
 		parent::initializeView($view);
 
 		// $view->Smarty->registerPlugin('function', 'categorylink', array($this, 'smarty_categorylink'));
+	}
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Mvc\RequestInterface $request
+	 * @param \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response
+	 * @return void
+	 * @throws \Exception
+	 * @override \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+	 * @see http://nerdcenter.de/extbase-fehlerbehandlung/
+	 */
+	public function processRequest(\TYPO3\CMS\Extbase\Mvc\RequestInterface $request, \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response) {
+		try {
+			parent::processRequest($request, $response);
+		} catch(PropertyException $exception) {
+			// If the property mapper did throw a \TYPO3\CMS\Extbase\Property\Exception, because it was unable to find the requested entity, call the page-not-found handler.
+			$previousException = $exception->getPrevious();
+			if ($previousException instanceof PropertyTargetNotFoundException || $previousException instanceof PropertyInvalidSourceException) {
+				$GLOBALS['TSFE']->pageNotFoundAndExit($this->entityNotFoundMessage);
+			}
+			throw $exception;
+		}
 	}
 }
