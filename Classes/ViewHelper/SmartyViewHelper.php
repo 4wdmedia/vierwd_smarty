10ebc913c7a0a02a4e71d6ced5534dff138aeb67
Robert Vock <robert.vock@4wdmedia.de>
2016-02-19 09:22:10 +0100

    Allow overriding of template variables and pass them along to included partials and layouts

--- Classes/ViewHelper/SmartyViewHelper.php
+++ Classes/ViewHelper/SmartyViewHelper.php
@@ -26,8 +26,41 @@ class SmartyViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelp
 		$view->setTemplateRootPaths($parentView->getTemplateRootPaths());
 		$view->setParentView($parentView);
 		$view->initializeView();
-		$view->assignMultiple($this->renderingContext->getTemplateVariableContainer()->getAll());
 
-		return $view->render($template);
+		if (!$view->isTopLevel) {
+			$isTopLevel = true;
+			$view->isTopLevel = true;
+			$smartyVariables = [];
+		} else {
+			$isTopLevel = false;
+			$smartyVariables = $view->Smarty->getTemplateVars();
+		}
+
+		$templateVariableContainer = $this->renderingContext->getTemplateVariableContainer();
+		$variables = $templateVariableContainer->getAll();
+		foreach ($variables as $key => &$value) {
+			if (!$view->isTopLevel || !isset($smartyVariables[$key])) {
+				$view->Smarty->assignByRef($key, $value);
+			}
+			unset($value);
+		}
+
+		$result = $view->render($template);
+
+		// if the variables have changed in a template, update the original variables in the outer templateVariableContainer
+		// It's not possible to really assign them by reference
+		$variablesAfterRendering = $view->Smarty->getTemplateVars();
+		foreach ($variables as $key => $value) {
+			if (isset($variablesAfterRendering[$key])) {
+				$templateVariableContainer->remove($key);
+				$templateVariableContainer->add($key, $value);
+			}
+		}
+
+		if ($isTopLevel) {
+			$view->isTopLevel = false;
+		}
+
+		return $result;
 	}
 }
