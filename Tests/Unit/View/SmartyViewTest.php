b0ff361d0b14ee53e46b697ec593026d7c7389f3
Robert Vock <robert.vock@4wdmedia.de>
2017-03-15 11:45:50 +0100

    Use new TYPO3 testing framework

--- Tests/Unit/View/SmartyViewTest.php
+++ Tests/Unit/View/SmartyViewTest.php
@@ -3,7 +3,7 @@
 namespace Vierwd\VierwdSmarty\Tests\Unit\View;
 
 use Vierwd\VierwdSmarty\View\SmartyView;
-use TYPO3\CMS\Core\Tests\UnitTestCase;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Web\Request as WebRequest;
 use TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext;
