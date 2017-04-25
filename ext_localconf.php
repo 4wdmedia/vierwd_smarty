ceadea6c6f41b2ad96c812daca0f27932c1daf58
Robert Vock <robert.vock@4wdmedia.de>
2017-04-25 10:08:45 +0200

    Fixed rendering of content elements in TYPO3 8

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 // Define TypoScript as content rendering template
 $GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'vierwdsmarty/Configuration/TypoScript/';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['contentRenderingTemplates'][] = 'vierwdsmarty/Configuration/TypoScript/v8/';
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	$_EXTKEY,
