3db954b23f34aec35403d28caf5116d44a58c301
Robert Vock <robert.vock@4wdmedia.de>
2017-07-24 17:10:33 +0200

    Fixed error 'The default controller for extension "VierwdSmarty" and plugin "smarty_render" can not be determined.' when the content element has switchableControllerActions in a flexform

---
+++ Classes/Bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vierwd\VierwdSmarty;
+
+class Bootstrap {
+
+	/**
+	 * Back reference to the parent content object
+	 * This has to be public as it is set directly from TYPO3
+	 *
+	 * @var \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer
+	 */
+	public $cObj;
+
+	public function run($content, $configuration) {
+		if ($this->cObj->data['pi_flexform'] && preg_match('/<field index="switchableControllerActions">/', $this->cObj->data['pi_flexform'])) {
+			// remove switchable controller actions
+			$this->cObj->data['pi_flexform'] = preg_replace('/<field index="switchableControllerActions">(.*?)<\\/field>/s', '', $this->cObj->data['pi_flexform']);
+		}
+		return $this->cObj->callUserFunction('TYPO3\\CMS\\Extbase\\Core\\Bootstrap->run', $configuration, $content);
+	}
+}
