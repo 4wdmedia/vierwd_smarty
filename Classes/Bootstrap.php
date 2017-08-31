01ae49d5f4f5bc1ec6bb8a700189f59c717d8b48
Robert Vock <robert.vock@4wdmedia.de>
2017-08-31 14:17:31 +0200

    Fixed error with removing of switchableControllerActions for older projects

--- Classes/Bootstrap.php
+++ Classes/Bootstrap.php
@@ -13,10 +13,19 @@ class Bootstrap {
 	public $cObj;
 
 	public function run($content, $configuration) {
+		$piFlexformBackup = false;
 		if (is_string($this->cObj->data['pi_flexform']) && preg_match('/<field index="switchableControllerActions">/', $this->cObj->data['pi_flexform'])) {
 			// remove switchable controller actions
+			$piFlexformBackup = $this->cObj->data['pi_flexform'];
 			$this->cObj->data['pi_flexform'] = preg_replace('/<field index="switchableControllerActions">(.*?)<\\/field>/s', '', $this->cObj->data['pi_flexform']);
 		}
-		return $this->cObj->callUserFunction('TYPO3\\CMS\\Extbase\\Core\\Bootstrap->run', $configuration, $content);
+
+		$result = $this->cObj->callUserFunction('TYPO3\\CMS\\Extbase\\Core\\Bootstrap->run', $configuration, $content);
+
+		if ($piFlexformBackup) {
+			$this->cObj->data['pi_flexform'] = $piFlexformBackup;
+		}
+
+		return $result;
 	}
 }
