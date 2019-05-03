809ffa33798637e9f3d66c63c9fd7e1a2a8e6cc5
Robert Vock <robert.vock@4wdmedia.de>
2019-05-03 14:39:54 +0200

    Fixed registering of caches to clear them with TYPO3 Console

--- ext_localconf.php
+++ ext_localconf.php
@@ -22,23 +22,18 @@ unset($menuContentObjectFactory);
 // ClearCache action
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] = \Vierwd\VierwdSmarty\Cache\ClearCacheHook::class;
 
-$cacheManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Cache\CacheManager::class);
-$smartyCaching = [
-	'vierwd_smarty_cache' => [
-		'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-		'backend' => \Vierwd\VierwdSmarty\Cache\CacheBackend::class,
-		'options' => ['cacheType' => 'cache'],
-		'groups' => ['all', 'pages', 'vierwd_smarty'],
-	],
-	'vierwd_smarty_compile' => [
-		'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-		'backend' => \Vierwd\VierwdSmarty\Cache\CacheBackend::class,
-		'options' => ['cacheType' => 'templates_c'],
-		'groups' => ['all', 'pages', 'vierwd_smarty'],
-	],
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['vierwd_smarty_cache'] = [
+	'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+	'backend' => \Vierwd\VierwdSmarty\Cache\CacheBackend::class,
+	'options' => ['cacheType' => 'cache'],
+	'groups' => ['all', 'pages', 'vierwd_smarty'],
 ];
-$cacheManager->setCacheConfigurations($smartyCaching);
-unset($cacheManager, $smartyCaching);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['vierwd_smarty_compile'] = [
+	'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+	'backend' => \Vierwd\VierwdSmarty\Cache\CacheBackend::class,
+	'options' => ['cacheType' => 'templates_c'],
+	'groups' => ['all', 'pages', 'vierwd_smarty'],
+];
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'Vierwd\\VierwdSmarty\\Cache\\ClearCacheHook->clear';
 
