6ed485b719db96d2bc2712d676d34ac56b2bb05b
Robert Vock <robert.vock@4wdmedia.de>
2021-09-17 17:52:14 +0200

    Moved calls to ExtensionUtility and ExtensionManagementUtility to TCA Overrides

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3') || exit;
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('vierwd_smarty', 'Configuration/TypoScript', 'Smarty Content Elements');
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3') || exit;
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+	'vierwd_smarty',
+	'smarty_render',
+	'Smarty Rendering'
+);
