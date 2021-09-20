074cf8fdd59957e1306d4536d02a3dc132736393
Robert Vock <robert.vock@4wdmedia.de>
2021-09-20 09:04:19 +0200

    Revert "Updated clearCacheHook"
    This reverts commit 56118dbf71b495afdb04b5f6a924701f7a9ff694.
    ModifyClearCacheActionsEvent is not available in TYPO3 v10

--- Classes/Cache/ClearCacheHook.php
+++ Classes/Cache/ClearCacheHook.php
@@ -3,30 +3,33 @@ declare(strict_types = 1);
 
 namespace Vierwd\VierwdSmarty\Cache;
 
-use TYPO3\CMS\Backend\Backend\Event\ModifyClearCacheActionsEvent;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Toolbar\ClearCacheActionsHookInterface;
 use TYPO3\CMS\Core\Cache\CacheManager;
 use TYPO3\CMS\Core\Cache\Exception\NoSuchCacheGroupException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class ClearCacheHook {
+class ClearCacheHook implements ClearCacheActionsHookInterface {
 
 	/**
 	 * Adds the option to clear the Smarty Template cache in the backend clear cache menu.
+	 *
+	 * @param array $cacheActions
+	 * @param array $optionValues
+	 * @phpstan-return void
 	 */
-	public function __invoke(ModifyClearCacheActionsEvent $event): void {
-		// &$cacheActions, &$optionValues
+	public function manipulateCacheActions(&$cacheActions, &$optionValues) {
 		if ($GLOBALS['BE_USER']->isAdmin()) {
 			$uriBuilder = GeneralUtility::makeInstance(UriBuilder::class);
 			$uri = $uriBuilder->buildUriFromRoute('tce_db', ['cacheCmd' => 'vierwd_smarty']);
-			$event->addCacheAction([
+			$cacheActions[] = [
 				'id' => 'vierwd_smarty',
 				'title' => 'LLL:EXT:vierwd_smarty/Resources/Private/Language/locallang.xlf:flushTemplateCache',
 				'description' => 'LLL:EXT:vierwd_smarty/Resources/Private/Language/locallang.xlf:flushTemplateCache.description',
 				'href' => $uri,
 				'iconIdentifier' => 'actions-system-cache-clear-impact-medium',
-			]);
-			$event->addCacheActionIdentifier('vierwd_smarty');
+			];
+			$optionValues[] = 'vierwd_smarty';
 		}
 	}
 
