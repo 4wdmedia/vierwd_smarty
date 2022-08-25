218e901fe0a64668de5ec83335fe139f83f0ec5d
Robert Vock <robert.vock@4wdmedia.de>
2022-08-25 14:56:17 +0200

    Updated code style for Smarty functions.

--- Resources/Private/Smarty/function.getCrop.php
+++ Resources/Private/Smarty/function.getCrop.php
@@ -1,21 +1,23 @@
 <?php
+declare(strict_types = 1);
 
+use TYPO3\CMS\Core\Imaging\ImageManipulation\Area;
 use TYPO3\CMS\Core\Imaging\ImageManipulation\CropVariantCollection;
 
 /**
  * Usage: {$crop = {getCrop image=$resource cropArea=default}}
  */
-function smarty_function_getCrop($params, $smarty) {
+function smarty_function_getCrop(array $params): ?Area {
 	$imageOrReference = $params['image'];
 
 	if (!$imageOrReference->getProperty('crop')) {
-		return '';
+		return null;
 	}
 
 	$crop = $imageOrReference->getProperty('crop');
 	if ($crop) {
 		$cropVariantCollection = CropVariantCollection::create($crop);
-		$cropAreaName = $params['cropArea'] ?: 'default';
+		$cropAreaName = $params['cropArea'] ?? 'default';
 		$cropArea = $cropVariantCollection->getCropArea($cropAreaName);
 		if ($cropArea->isEmpty()) {
 			$crop = null;
