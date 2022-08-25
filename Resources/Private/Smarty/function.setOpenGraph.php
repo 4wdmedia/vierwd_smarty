f239f6facb612cc69840f9af56921c5770ead201
Robert Vock <robert.vock@4wdmedia.de>
2022-08-25 14:56:17 +0200

    Updated code style for Smarty functions.

--- Resources/Private/Smarty/function.setOpenGraph.php
+++ Resources/Private/Smarty/function.setOpenGraph.php
@@ -1,10 +1,11 @@
 <?php
+declare(strict_types = 1);
 
 /**
  * set a global value which can be used in TypoScript with `data = global:OpenGraph|description`
  */
-function smarty_function_setOpenGraph($params, $smarty) {
-	if (!$GLOBALS['OpenGraph']) {
+function smarty_function_setOpenGraph(array $params): void {
+	if (empty($GLOBALS['OpenGraph'])) {
 		$GLOBALS['OpenGraph'] = [];
 	}
 
